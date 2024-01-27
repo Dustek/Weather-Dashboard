03d63e6a2551b00f5f37429136b2500a0e610e80
Vytas <v.venckas@gmail.com>
2024-01-27 14:33:12 +0000

    added logic to get data for 5 forecast days

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ fetch(queryURLcoordinate)
    lat = data[0].lat
    lon = data[0].lon
    getCurrentWeather(lat, lon)
+   getFutureWeather(lat, lon)
 })
 
 
@@ -24,16 +25,46 @@ fetch(queryURL)
 })
 .then(function (data) {
     console.log(data)
-    var temperature = data.list[0].main.temp;
-    var humidity = data.list[0].main.humidity;
-    var wind = data.list[0].wind.speed
-    var date = data.list[0].dt * 1000
+    displayWeatherData(0, $("#today"), data);
+});
+}
 
-    var tempDiv = $("<p>").text("temp: " + temperature + "°C")
-    var humidityDiv = $("<p>").text("humidity: " + humidity + "%");
-    var windDiv = $("<p>").text("wind: " + wind+ " KPH");
-    var dateDiv = $("<p>").text("date: " + dayjs(date).format("DD/MM/YYYY"));
+function getFutureWeather(lat, lon) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=metric&appid=19bf45fa62f0e7c4bb3694a3eda4303a";
+    fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        for (var i = 7; i < data.list.length; i += 8 ) {
+            var dayDiv = $("<div>").addClass("forecast-day");
+            displayWeatherData(i, dayDiv, data);
+            $("#forecast").append(dayDiv);
+        }
+    });
+    }
 
-    $("#today").append(tempDiv, humidityDiv, windDiv, dateDiv);
 
-})}
+
+
+    function displayWeatherData (index, displayDiv, data) {
+        var temperature = data.list[index].main.temp;
+        var humidity = data.list[index].main.humidity;
+        var wind = data.list[index].wind.speed
+        var date = data.list[index].dt * 1000
+        // displays data in correct formats
+        var tempDiv = $("<p>").text("temp: " + temperature + "°C")
+        var humidityDiv = $("<p>").text("humidity: " + humidity + "%");
+        var windDiv = $("<p>").text("wind: " + wind+ " KPH");
+        var dateDiv = $("<p>").text("date: " + dayjs(date).format("DD/MM/YYYY"));
+        displayDiv.append(tempDiv, humidityDiv, windDiv, dateDiv);
+
+    }
+
+
+
+
+
+
+
