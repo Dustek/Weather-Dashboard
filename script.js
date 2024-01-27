7f264c0dfb90fe2f28ffa085cd727d51dc1ea77c
Vytas <v.venckas@gmail.com>
2024-01-27 14:01:02 +0000

    fetched and displayed data for current weather

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ fetch(queryURLcoordinate)
 .then(function (data) {
    lat = data[0].lat
    lon = data[0].lon
-   useCoordinates(lat, lon)
+   getCurrentWeather(lat, lon)
 })
 
 
 // fetches weather API based on coordinates
-function useCoordinates(lat, lon) {
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=19bf45fa62f0e7c4bb3694a3eda4303a";
+function getCurrentWeather(lat, lon) {
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=metric&appid=19bf45fa62f0e7c4bb3694a3eda4303a";
 fetch(queryURL)
 .then(function (response) {
   return response.json();
@@ -26,8 +26,14 @@ fetch(queryURL)
     console.log(data)
     var temperature = data.list[0].main.temp;
     var humidity = data.list[0].main.humidity;
-    var wind = data.list[0].main.wind.speed
-    var date = data.list[0].dt
+    var wind = data.list[0].wind.speed
+    var date = data.list[0].dt * 1000
 
+    var tempDiv = $("<p>").text("temp: " + temperature + "°C")
+    var humidityDiv = $("<p>").text("humidity: " + humidity + "%");
+    var windDiv = $("<p>").text("wind: " + wind+ " KPH");
+    var dateDiv = $("<p>").text("date: " + dayjs(date).format("DD/MM/YYYY"));
+
+    $("#today").append(tempDiv, humidityDiv, windDiv, dateDiv);
 
 })}
