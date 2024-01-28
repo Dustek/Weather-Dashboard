53e134814f9c3cb4287909b661493022dd09e364
Vytas <v.venckas@gmail.com>
2024-01-28 21:01:28 +0000

    added style and fixed some bugs

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ var lon
 $("#search-form").on("submit", function(event) {
     event.preventDefault();
     var userinput = $("#search-input").val();
+    if (userinput.trim() === "") {
+        alert("Please enter a city name");
+        return;
+    }
     $("#today").empty()
     $("#forecast").empty()
     getWeather(userinput);
 // Creates interactive buttons from user input history
     var newButton = $("<button>")
-    newButton.addClass("btn")
+    newButton.addClass("btn btn-primary")
     newButton.text(userinput)
     newButton.on("click", function() {
         $("#today").empty()
@@ -20,12 +24,16 @@ $("#search-form").on("submit", function(event) {
 
     saveSearchHistory();
     $("#search-input").val("")
+    $("#forecast-title").removeClass("hidden");
+    $("#city-name").text(userinput)
+    $("#city-name").removeClass("hidden");
 });
 
 // Saves button history to local storage
 function saveSearchHistory() {
     var searchHistory = $("#history").html();
     localStorage.setItem("searchHistory", searchHistory);
+    attachEventListeners();
 }
 
 // Loads button history on page load
@@ -33,9 +41,24 @@ function loadSearchHistory() {
     var searchHistory = localStorage.getItem("searchHistory");
     if (searchHistory) {
         $("#history").html(searchHistory);
+        attachEventListeners();
     }
 }
 loadSearchHistory();
+
+
+function attachEventListeners() {
+    $(".btn-primary").on("click", function() {
+        $("#today").empty();
+        $("#forecast").empty();
+        var cityName = $(this).text();
+        getWeather(cityName);
+        $("#search-input").val("");
+        $("#forecast-title").removeClass("hidden");
+        $("#city-name").text(cityName);
+        $("#city-name").removeClass("hidden");
+    });
+}
 
 $("#clear").on("click", function(){
 localStorage.clear()
@@ -101,10 +124,11 @@ function getFutureWeather(lat, lon) {
         var humidity = data.list[index].main.humidity;
         var wind = data.list[index].wind.speed;
         var date = data.list[index].dt * 1000;
+        var formattedDate = dayjs(date).format("DD/MM/YYYY");
+        var dateDiv = $("<p>").addClass("date").text(formattedDate).css("font-weight", "bold");
         var cloudsDiv =$("<p>").text(cloudIcon);
         var tempDiv = $("<p>").text("temp: " + temperature + "°C");
         var humidityDiv = $("<p>").text("humidity: " + humidity + "%");
         var windDiv = $("<p>").text("wind: " + wind + " KPH");
-        var dateDiv = $("<p>").text("date: " + dayjs(date).format("DD/MM/YYYY"));
-        displayDiv.append(cloudsDiv, tempDiv, humidityDiv, windDiv, dateDiv);
+        displayDiv.append(dateDiv, cloudsDiv, tempDiv, humidityDiv, windDiv);
     }
